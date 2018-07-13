c5fef8f1a223369e2833715541392d3948218053
Will Dietz <w@wdtz.org>
2018-07-13 14:03:04 -0500

    refactor a touch

--- default.nix
+++ default.nix
@@ -30,18 +30,19 @@ let
 
   normalHaskellPackages = pkgs.haskell.packages.${compiler};
 
+  statify = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (justStaticExecutables drv) [
+        "--ghc-option=-optl=-static"
+        "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
+        "--extra-lib-dirs=${pkgs.zlib.static}/lib"
+  ];
+
   haskellPackages = with pkgs.haskell.lib; normalHaskellPackages.override {
     overrides = self: super: {
       hpc-coveralls = appendPatch super.hpc-coveralls (builtins.fetchurl https://github.com/guillaume-nargeot/hpc-coveralls/pull/73/commits/344217f513b7adfb9037f73026f5d928be98d07f.patch);
 
-      cachix = appendConfigureFlag super.cachix "--ghc-option=-optl=-static";
-      hello = pkgs.lib.foldl appendConfigureFlag super.hello [
-        "--ghc-option=-optl=-static"
-        "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
-        "--extra-lib-dirs=${pkgs.zlib.static}/lib"
-      ];
-
+      cachix = statify super.cachix;
       blank-me-up = super.callPackage f {}; # XXX ?!
+      hello = statify super.hello;
     };
   };
 
