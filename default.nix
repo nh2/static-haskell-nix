060ae505001d404faa54cbd5ab846116b3ca027a
Will Dietz <w@wdtz.org>
2018-07-13 13:31:28 -0500

    default.nix: leave aeson alone
    No needed in this configuration,
    and I think only needed when using
    the bootstrap compiler which is only
    patched to be "good enough" AFAICT.

--- default.nix
+++ default.nix
@@ -32,12 +32,6 @@ let
 
   haskellPackages = with pkgs.haskell.lib; normalHaskellPackages.override {
     overrides = self: super: {
-      # Without this, we get an error when haddock is executed on aeson:
-      #   <command line>: can't load .so/.DLL for: libgmp.so (libgmp.so: cannot open shared object file: No such file or directory)
-      #   builder for '/nix/store/3x9abjx43jn2fg4h5av2vk0igmwv67xs-aeson-1.2.4.0-x86_64-unknown-linux-musl.drv' failed with exit code 1
-      # Note sure yet why it's trying to use libgmp.so when executing haddock.
-      aeson = dontHaddock super.aeson;
-
       hpc-coveralls = appendPatch super.hpc-coveralls (builtins.fetchurl https://github.com/guillaume-nargeot/hpc-coveralls/pull/73/commits/344217f513b7adfb9037f73026f5d928be98d07f.patch);
     };
   };
