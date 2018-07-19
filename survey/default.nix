a0f767252490430fd6ff5924a438fd18fa0f4274
Niklas Hambüchen <mail@nh2.me>
2018-07-19 05:41:24 +0200

    WIP: Cleanups and all packages work

--- survey/default.nix
+++ survey/default.nix
@@ -1,9 +1,4 @@
 let
-  # validity-overlay = let pkgs = import <nixpkgs> {}; in import (
-  #   (pkgs.pkgsMusl.fetchFromGitHub (import /home/niklas/src/haskell/intray/validity-version.nix)
-  #   + "/overlay.nix")
-  # );
-
   # TODO: Remove when https://github.com/NixOS/cabal2nix/pull/360 is merged and available
   cabal2nix-fix-overlay = final: previous:
     with final.haskell.lib; {
@@ -25,114 +20,81 @@ let
       });
     };
 
+  # validity-overlay = let pkgs = import <nixpkgs> {}; in import (
+  #   (pkgs.pkgsMusl.fetchFromGitHub (import /home/niklas/src/haskell/intray/validity-version.nix)
+  #   + "/overlay.nix")
+  # );
+
+  # TODO Get this from Github
+  # Uses validity commit 4bd822c8 (nh2's branch `nix-improvements`)
   validity-overlay = import /home/niklas/src/haskell/validity/overlay.nix;
   pkgs = (import <nixpkgs> {
+    # TODO Get this from Github
+    # Uses intray commit 3407a33f (nh2's branch `fix-stale-cabal-files-not-rerunning-hpack-servant-0.14`)
     overlays = [ cabal2nix-fix-overlay validity-overlay (import /home/niklas/src/haskell/intray/overlay.nix) ];
     config.allowUnfree = true;
   }).pkgsMusl;
 
 in
 
-# { nixpkgs ? syd-pkgs.pkgsMusl, compiler ? "ghc843" }:
 { compiler ? "ghc843" }:
 
 
 let
 
-  # pkgs = syd-pkgs.pkgsMusl;
-  # pkgs = nixpkgs.pkgsMusl;
-
-  # normalHaskellPackages = builtins.trace ("${pkgs.haskellPackages.intray-server.buildPhase}") pkgs.haskell.packages.${compiler};
-  # normalHaskellPackages = pkgs.haskell.packages.${compiler};
   normalHaskellPackages = pkgs.haskellPackages;
 
-  pic = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (drv) [
-    "--ghc-option=-fPIC"
-  ];
-  statify = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (disableLibraryProfiling (disableSharedExecutables (disableSharedLibraries drv))) [
-  # statify = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (disableLibraryProfiling (disableSharedExecutables drv)) [
-        "--ghc-option=-static"
-        "--ghc-option=-optl=-static"
-        # "--ghc-option=-fPIC"
-        "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
-        "--extra-lib-dirs=${pkgs.zlib.static}/lib"
-        # XXX: This doesn't actually work "yet":
-        # * first, it helps to not remove the static libraries: https://github.com/dtzWill/nixpkgs/commit/54a663a519f622f19424295edb55d01686261bb4 (should be sent upstream)
-        # * second, ghc wants to link against libtinfo but no static version of that is built
-        #   (actually no shared either, we create symlink for it-- I think)
-        "--extra-lib-dirs=${pkgs.ncurses.override { enableStatic = true; }}/lib"
-  ];
+  sqlite_static = pkgs.sqlite.overrideAttrs (old: { dontDisableStatic = true; });
 
-  # half_statify = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (disableLibraryProfiling (disableSharedExecutables (disableSharedLibraries drv))) [
-
-  # Using .out because "sqlite" is the package that contains only the sqlite3 binary, not the library.
-  sqlite_static = (pkgs.sqlite.overrideAttrs (old: { dontDisableStatic = true; })).out;
-  sqlite_static_full = pkgs.sqlite.overrideAttrs (old: { dontDisableStatic = true; });
-
-  # useSqliteWithStaticSupport = drv: with pkgs.haskell.lib; appendConfigureFlag drv [
-  #   "--extra-lib-dirs=${sqlite_static}/lib"
-  # ];
-
-  linkSqliteStatically = drv: with pkgs.haskell.lib; appendConfigureFlag drv [
-    "--extra-lib-dirs=${sqlite_static}/lib"
-    # TODO: Not great, instead of this we should make sure that
-    # persistent-sqlite actually depends on the static version
-    "--ghc-option=-optl=-l:libsqlite3.a"
-  ];
-
-
+  lzma_static = pkgs.lzma.overrideAttrs (old: { dontDisableStatic = true; });
 
   haskellPackages = with pkgs.haskell.lib; normalHaskellPackages.override (old: {
     overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super:
     let
-      # Cabal_dedupe_src = pkgs.fetchFromGitHub {
-      #   owner = "nh2";
-      #   repo = "cabal";
-      #   rev = "7cb409fe7433833a3a8aa4b38a5fb3c2e01a5e5d";
-      #   sha256 = "0qhq80mfm55fbnzk8p6ar13b954wwd2y9c9pxlsh6rmaa91rd6y0";
-      # };
-      Cabal_dedupe_src =
-        let
-          filter = name: type:
-            let baseName = baseNameOf (toString name); in
-            !( (type == "directory" && baseName == ".stack-work") );
-        in pkgs.lib.cleanSourceWith { inherit filter; src = /home/niklas/src/haskell/cabal; };
+      # TODO do this via patches instead
+      cabal_patched_src = pkgs.fetchFromGitHub {
+        owner = "nh2";
+        repo = "cabal";
+        rev = "748f07b50724f2618798d200894f387020afc300";
+        sha256 = "1k559m291f6spip50rly5z9rbxhfgzxvaz64cx4jqpxgfhbh2gfs";
+      };
 
-      Cabal_dedupe_subdir = pkgs.stdenv.mkDerivation {
+      Cabal_patched_Cabal_subdir = pkgs.stdenv.mkDerivation {
         name = "cabal-dedupe-src";
         buildCommand = ''
-          cp -rv ${Cabal_dedupe_src}/Cabal/ $out
+          cp -rv ${cabal_patched_src}/Cabal/ $out
         '';
       };
 
-      Cabal_dedupe = self.callCabal2nix "Cabal" Cabal_dedupe_subdir {};
+      Cabal_patched = self.callCabal2nix "Cabal" Cabal_patched_Cabal_subdir {};
 
       useFixedCabal = drv: overrideCabal drv (old: {
-        libraryHaskellDepends = old.libraryHaskellDepends ++ [ Cabal_dedupe ];
-        setupHaskellDepends = [ Cabal_dedupe ];
+        setupHaskellDepends = (if old ? setupHaskellDepends then old.setupHaskellDepends else []) ++ [ Cabal_patched ];
+        # TODO Check if this is necessary
+        libraryHaskellDepends = (if old ? libraryHaskellDepends then old.libraryHaskellDepends else []) ++ [ Cabal_patched ];
       });
 
-      proper_statify = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (disableLibraryProfiling (disableSharedExecutables (useFixedCabal drv))) [
-            # "--ghc-option=-fPIC"
-            "--enable-executable-static" # requires `useFixedCabal`
-            "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
-            "--extra-lib-dirs=${pkgs.zlib.static}/lib"
-            # XXX: This doesn't actually work "yet":
-            # * first, it helps to not remove the static libraries: https://github.com/dtzWill/nixpkgs/commit/54a663a519f622f19424295edb55d01686261bb4 (should be sent upstream)
-            # * second, ghc wants to link against libtinfo but no static version of that is built
-            #   (actually no shared either, we create symlink for it-- I think)
-            "--extra-lib-dirs=${pkgs.ncurses.override { enableStatic = true; }}/lib"
+      statify = drv: with pkgs.haskell.lib; pkgs.lib.foldl appendConfigureFlag (disableLibraryProfiling (disableSharedExecutables (useFixedCabal drv))) [
+        # "--ghc-option=-fPIC"
+        "--enable-executable-static" # requires `useFixedCabal`
+        "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
+        # TODO These probably shouldn't be here but only for packages that actually need them
+        "--extra-lib-dirs=${pkgs.zlib.static}/lib"
+        "--extra-lib-dirs=${pkgs.ncurses.override { enableStatic = true; }}/lib"
       ];
 
     in
     {
-  # haskellPackages = with pkgs.haskell.lib; normalHaskellPackages.extend (self: super: {
-    # Helpers for other packages
+      # Helpers for other packages
 
       hpc-coveralls = appendPatch super.hpc-coveralls (builtins.fetchurl https://github.com/guillaume-nargeot/hpc-coveralls/pull/73/commits/344217f513b7adfb9037f73026f5d928be98d07f.patch);
-      # TODO: This doesn't seem to be picked up by our overlays, they seem to use non-static sqlite
-      # persistent-sqlite = builtins.trace "here" useSqliteWithStaticSupport (super.persistent-sqlite.override { sqlite = sqlite_static_full; });
-      persistent-sqlite = super.persistent-sqlite.override { sqlite = sqlite_static_full; };
+      persistent-sqlite = super.persistent-sqlite.override { sqlite = sqlite_static; };
+      lzma = super.lzma.override { lzma = lzma_static; };
+      # If we `useFixedCabal` on stack, we also need to use the
+      # it on hpack and hackage-security because otherwise
+      # stack depends on 2 different versions of Cabal.
+      hpack = useFixedCabal super.hpack;
+      hackage-security = useFixedCabal super.hackage-security;
 
       # Static executables that work
 
@@ -142,31 +104,17 @@ let
       cabal-install = statify super.cabal-install;
       bench = statify super.bench;
 
-      # intray-server = linkSqliteStatically (statify super.intray-server);
-      # intray-web-server = linkSqliteStatically (statify super.intray-web-server);
-      # intray-server = useFixedCabal super.intray-server;
       intray-server = super.intray-server;
-      # intray-server-test-utils = useFixedCabal super.intray-server-test-utils;
-      intray-server-test-utils = super.intray-server-test-utils;
-      # intray-web-server = useFixedCabal (linkSqliteStatically (statify super.intray-web-server));
-      # intray-web-server = statify super.intray-web-server;
-      intray-web-server = proper_statify super.intray-web-server;
+      intray-web-server = statify super.intray-web-server;
+
+      stack = useFixedCabal (statify super.stack);
+
+      dhall = statify super.dhall;
+
+      cachix = appendConfigureFlag (statify super.cachix) [ "--ghc-option=-j1" ];
 
       # Static executables that don't work yet
 
-      # stack = appendConfigureFlag (statify super.stack) [ "--ghc-option=-j1" ];
-      # stack = overrideCabal (appendConfigureFlag (half_statify super.stack) [ "--ghc-option=-j1" ]) (old: {
-      #   src = /home/niklas/src/haskell/stack-small;
-      # });
-      # If we `useFixedCabal` on stack, we also need to use the
-      # it on hpack and hackage-security because otherwise
-      # stack depends on 2 different versions of Cabal.
-      hpack = useFixedCabal super.hpack;
-      hackage-security = useFixedCabal super.hackage-security;
-      # stack = useFixedCabal (half_statify super.stack);
-      stack = proper_statify super.stack;
-      cachix = statify super.cachix;
-      dhall = statify super.dhall;
     });
   });
 
@@ -175,25 +123,25 @@ in
     working = {
       inherit (haskellPackages)
         hello
+        stack
         hlint
         ShellCheck
         cabal-install
         bench
+        dhall
+        cachix
         ;
     };
 
     notWorking = {
       inherit (haskellPackages)
-        stack
-        dhall
-        cachix
         ;
     };
 
     all = working // notWorking;
 
     inherit haskellPackages;
-    inherit sqlite_static;
   }
 
-# TODO Update README to ensure mention that I use nixpkgs 762dc9d (which has some patches picked)
+# TODO Update README to depend on nixpkgs master I use, and write something that picks patches I use on top here
+# TODO Instead of picking https://github.com/NixOS/nixpkgs/pull/43713, use a Python script to dedupe `-L` flags from the NIX_*LDFLAGS variables
