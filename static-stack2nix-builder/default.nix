7609657fe9e81e2109045e091fc862823eaea0a4
Niklas Hambüchen <mail@nh2.me>
2019-05-23 01:22:00 +0200

    Simplify and improve static-statck2nix-builder

--- static-stack2nix-builder/default.nix
+++ static-stack2nix-builder/default.nix
@@ -13,76 +13,99 @@
 # the second `nix-build` invocation, because as of writing, `stack2nix`
 # clutters the projects source directory with generated `.cabal` files.
 {
-  # The name of the cabal package to build.
+  # The name of the cabal package to build, e.g. "pandoc".
   cabalPackageName ? "myproject",
+
+  # Compiler name in nixpkgs, e.g. "ghc86".
+  # Must match the one in the `resolver` in `stack.yaml`.
+  # TODO: Make `stack2nix` tell us that.
+  compiler ? "ghc864",
 
   # Hackage snapshot time to pass to `stack2nix`.
   # This determines the versions of package revisions in stack `extra-deps`
   # that are not pinned to a revision.
   hackageSnapshot ? "2019-05-08T00:00:00Z",
 
-  # Path to directory in which `stack2nix` is run and into which it drops
-  # its output file.
-  # Made customisable so that other projects can import this script we're
-  # in here via URL.
-  runDir ? ./.,
+  # Path to directory containing `stack.yaml`.
+  stack2nix-stack-project-dir,
+
+  # String path to stack2nix shall drop its output.
+  stack2nix-output-path ? "/tmp/stack2nix-output.nix",
 
   # Pin nixpkgs version.
   normalPkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/88ae8f7d55efa457c95187011eb410d097108445.tar.gz) {},
+
+  # Enable for faster building, but not proper releases.
+  disableOptimization ? false,
 }:
 let
-  # In `survey` we provide a nixpkgs set with some fixes; import it here.
-  pkgs = (import ../survey/default.nix {
-    inherit normalPkgs;
-  }).pkgs;
 
-  # Pin `stack2nix` version.
-  # TODO Use `pkgs.stack2nix` instead of this once `stack2nix` 0.2 is in `pkgs`
-  stack2nix_src = pkgs.fetchFromGitHub {
-    owner = "input-output-hk";
-    repo = "stack2nix";
-    rev = "v0.2.1";
-    sha256 = "1ihcp3mr0s89xmc81f9hxq07jw6pm3lixr5bdamqiin1skpk8q3b";
-  };
-  stack2nix = import (stack2nix_src + "/default.nix") {};
+  static-haskell-nix_pkgsMusl = (import ../survey/default.nix {
+    inherit normalPkgs;
+    inherit compiler;
+    inherit disableOptimization;
+  }).pkgs;
 
   # Script that runs `stack2nix` on a given stack project's source dir.
   # Arguments given to the script are given to `stack2nix`.
-  # Running the script creates file `stack2nix-output.nix`.
+  # Running the script creates the output file at `stack2nix-output-path`.
   stack2nix-script =
     # `stack2nix` requires `cabal` on $PATH.
     # We put our nixpkgs's version of `nix` on $PATH for reproducibility.
-    pkgs.writeScript "stack2nix-build-script.sh" ''
+    normalPkgs.writeScript "stack2nix-build-script.sh" ''
       #!/usr/bin/env bash
       set -eu -o pipefail
       export NIX_PATH=nixpkgs=${normalPkgs.path}
-      PATH=${pkgs.cabal-install}/bin:${normalPkgs.nix}/bin:$PATH ${stack2nix}/bin/stack2nix --hackage-snapshot "${hackageSnapshot}" -o stack2nix-output.nix $@
+      export PATH=${normalPkgs.cabal-install}/bin:${normalPkgs.nix}/bin:$PATH
+      set -x
+      ${normalPkgs.stack2nix}/bin/stack2nix "${stack2nix-stack-project-dir}" --hackage-snapshot "${hackageSnapshot}" -o "${stack2nix-output-path}" $@
     '';
 
-  # Builds static executables from a `stack2nix-output.nix` file generated
-  # with `stack2nix`.
-  static_package = (import ../survey/default.nix {
-    normalPkgs = pkgs;
-    normalHaskellPackages = import (runDir + "/stack2nix-output.nix") {
-      inherit pkgs;
+  stack2nix_output = import stack2nix-output-path { pkgs = static-haskell-nix_pkgsMusl; };
+
+  pkgs_with_stack2nix_packages_inside = static-haskell-nix_pkgsMusl.extend (final: previous: {
+    haskell = final.lib.recursiveUpdate previous.haskell {
+      packages."${compiler}" = stack2nix_output;
     };
-  }).haskellPackages."${cabalPackageName}";
+  });
+
+  haskell-static-nix_output = (import ../survey/default.nix {
+    normalPkgs = pkgs_with_stack2nix_packages_inside;
+    inherit compiler;
+    inherit disableOptimization;
+  });
+
+  static_package = haskell-static-nix_output.haskellPackages."${cabalPackageName}";
 
   # Script that runs `nix-build` to build the final executable.
   # We do this to fix the version of `nix` to the one in our nixpkgs
   # for reproducibility, as changing nix versions can change the build env.
   # Arguments given to the script are given to `nix-build`.
   static-build-script =
-    pkgs.writeScript "static-build-script.sh" ''
+    normalPkgs.writeScript "static-build-script.sh" ''
       #!/usr/bin/env bash
       set -eu -o pipefail
       set -x
-      ${normalPkgs.nix}/bin/nix-build --no-out-link -A static_package $@
+      ${normalPkgs.nix}/bin/nix-build --no-link -A static_package $@
+    '';
+
+  # Runs both `stack2nix` and does the static nix-build in one go.
+  # Arguments given to the script are given to `nix-build`.
+  run-stack2nix-and-static-build-script =
+    normalPkgs.writeScript "stack2nix-and-build-script.sh" ''
+      #!/usr/bin/env bash
+      set -eu -o pipefail
+      ${stack2nix-script}
+      ${static-build-script} $@
     '';
 
 in {
-  inherit pkgs;
+  inherit static-haskell-nix_pkgsMusl;
   inherit stack2nix-script;
+  inherit stack2nix_output;
+  inherit pkgs_with_stack2nix_packages_inside;
+  inherit haskell-static-nix_output;
   inherit static_package;
   inherit static-build-script;
+  inherit run-stack2nix-and-static-build-script;
 }
