9fea8116489f856dcdd7e36d60f8e34f6c6eba59
Niklas Hambüchen <mail@nh2.me>
2019-05-23 23:50:24 +0200

    Add static-stack2nix-builder-example

---
+++ static-stack2nix-builder-example/README.md
@@ -0,0 +1,9 @@
+# example-project
+
+Example stack-based project that shows how to use `static-stack2nix-builder`.
+
+## Usage in your project
+
+Copy the `default.nix` into your project, and adjust it as needed.
+
+It has instructions for running inside.
---
+++ static-stack2nix-builder-example/default.nix
@@ -0,0 +1,44 @@
+# Run using:
+#
+#     $(nix-build --no-link -A fullBuildScript)
+{
+  stack2nix-output-path ? "custom-stack2nix-output.nix",
+}:
+let
+  cabalPackageName = "example-project";
+  compiler = "ghc864"; # matching stack.yaml
+
+  # Pin nixpkgs version.
+  pkgs = import (fetchTarball https://github.com/nh2/nixpkgs/archive/a2d7e9b875e8ba7fd15b989cf2d80be4e183dc72.tar.gz) {};
+
+  # Pin static-haskell-nix version.
+  static-haskell-nix = fetchTarball https://github.com/nh2/static-haskell-nix/archive/de8346b794031a8104840e2e17193a15e26174a0.tar.gz;
+
+  stack2nix-script = import "${static-haskell-nix}/static-stack2nix-builder/stack2nix-script.nix" {
+    inherit pkgs;
+    stack-project-dir = toString ./.; # where stack.yaml is
+    hackageSnapshot = "2019-05-08T00:00:00Z"; # pins e.g. extra-deps without hashes or revisions
+  };
+
+  static-stack2nix-builder = import "${static-haskell-nix}/static-stack2nix-builder/default.nix" {
+    normalPkgs = pkgs;
+    inherit cabalPackageName compiler stack2nix-output-path;
+    # disableOptimization = true; # for compile speed
+  };
+
+  # Full invocation, including pinning `nix` version itself.
+  fullBuildScript = pkgs.writeScript "stack2nix-and-build-script.sh" ''
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    STACK2NIX_OUTPUT_PATH=$(${stack2nix-script})
+    ${pkgs.nix}/bin/nix-build --no-link -A static_package --argstr stack2nix-output-path "$STACK2NIX_OUTPUT_PATH" $@
+  '';
+
+in
+  {
+    static_package = static-stack2nix-builder.static_package;
+    inherit fullBuildScript;
+    # For debugging:
+    inherit stack2nix-script;
+    inherit static-stack2nix-builder;
+  }
