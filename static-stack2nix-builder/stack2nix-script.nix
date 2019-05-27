11f343f175be00a21d8b15063ec083b1fac0d79c
Niklas Hambüchen <mail@nh2.me>
2019-05-28 01:25:06 +0200

    static-stack2nix-builder: Address stack2nix needing wget.
    Fixes stac2nix error when running in `nixos/nix` docker container:
    
    		wget: unrecognized option: input-file=-
    		BusyBox v1.29.3 (2019-01-24 07:45:07 UTC) multi-call binary

--- static-stack2nix-builder/stack2nix-script.nix
+++ static-stack2nix-builder/stack2nix-script.nix
@@ -22,18 +22,30 @@
   # script prints only the final store path to stdout.
   # The output is generated to a `mktemp --directory` so that parallel
   # invocations don't influence each other.
-  # Note in this script we should qualify all executables from nix packages,
+  # Note in this script we should qualify all executables from nix packages
+  # (or put them on PATH accordingly)
   # as it's run in the user's shell (not in a normal nix build environment,
   # since `stack2nix` needs internet access), so we can't make any assumptions
   # about shell builtins or what's on PATH. For example, if `mktemp` is from
   # `busybox` instead of `coreutils`, it may not support the `--directory`
   # option.
+  # And for example the `nixos/nix` Docker container is minimal and supplies
+  # many executables from `busybox`, such as `mktemp` and `wget`.
+  let
+    # Shell utils called by stack2nix or the script itself:
+    add_to_PATH = [
+      "${pkgs.coreutils}/bin" # `mktemp` et al
+      "${pkgs.cabal-install}/bin" # `cabal`
+      "${pkgs.nix}/bin" # various `nix-*` commands
+      "${pkgs.wget}/bin" # `wget`
+    ];
+  in
   pkgs.writeScript "stack2nix-build-script.sh" ''
     #!/usr/bin/env bash
     set -eu -o pipefail
     export NIX_PATH=nixpkgs=${pkgs.path}
-    export PATH=${pkgs.cabal-install}/bin:${pkgs.nix}/bin:$PATH
-    OUT_DIR=$(${pkgs.coreutils}/bin/mktemp --directory -t stack2nix-output-dir.XXXXXXXXXX)
+    export PATH=${pkgs.lib.concatStringsSep ":" add_to_PATH}:$PATH
+    OUT_DIR=$(mktemp --directory -t stack2nix-output-dir.XXXXXXXXXX)
     set -x
     ${pkgs.stack2nix}/bin/stack2nix "${stack-project-dir}" --hackage-snapshot "${hackageSnapshot}" -o "$OUT_DIR/stack2nix-output.nix" $@ 1>&2
     nix-store --add "$OUT_DIR/stack2nix-output.nix"
