16b541aba7f4036e343ce94a313f3c41a8b899f7
Niklas Hambüchen <mail@nh2.me>
2019-05-28 01:09:17 +0200

    static-stack2nix-builder: Don't assume mktemp is from coreutils.
    Fixes issue seen when used in `nixos/nix` docker image:
    
    		building '/nix/store/ngsch8ridwjky8zvmdzl3j53sfw7831a-stack2nix-and-build-script.sh.drv'...
    		mktemp: unrecognized option: directory
    		BusyBox v1.29.3 (2019-01-24 07:45:07 UTC) multi-call binary.
    
    		Usage: mktemp [-dt] [-p DIR] [TEMPLATE]

--- static-stack2nix-builder/stack2nix-script.nix
+++ static-stack2nix-builder/stack2nix-script.nix
@@ -22,12 +22,18 @@
   # script prints only the final store path to stdout.
   # The output is generated to a `mktemp --directory` so that parallel
   # invocations don't influence each other.
+  # Note in this script we should qualify all executables from nix packages,
+  # as it's run in the user's shell (not in a normal nix build environment,
+  # since `stack2nix` needs internet access), so we can't make any assumptions
+  # about shell builtins or what's on PATH. For example, if `mktemp` is from
+  # `busybox` instead of `coreutils`, it may not support the `--directory`
+  # option.
   pkgs.writeScript "stack2nix-build-script.sh" ''
     #!/usr/bin/env bash
     set -eu -o pipefail
     export NIX_PATH=nixpkgs=${pkgs.path}
     export PATH=${pkgs.cabal-install}/bin:${pkgs.nix}/bin:$PATH
-    OUT_DIR=$(mktemp --directory -t stack2nix-output-dir.XXXXXXXXXX)
+    OUT_DIR=$(${pkgs.coreutils}/bin/mktemp --directory -t stack2nix-output-dir.XXXXXXXXXX)
     set -x
     ${pkgs.stack2nix}/bin/stack2nix "${stack-project-dir}" --hackage-snapshot "${hackageSnapshot}" -o "$OUT_DIR/stack2nix-output.nix" $@ 1>&2
     nix-store --add "$OUT_DIR/stack2nix-output.nix"
