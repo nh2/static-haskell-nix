8b281d990972733eaa0e5b2b3fa77eb76001f9e9
Niklas Hambüchen <mail@nh2.me>
2019-08-12 07:07:18 +0200

    stack2nix: Simplify CI and testing locally by using nixpkgs submodule.
    Also use stack2nix version that's
    
    * not marked as broken (the case in current nixpkgs master)
    * new enough to have the fix for the
    	`stack2nix-output.nix: commitBuffer: invalid argument (invalid character)`
    	problem, see
    	https://github.com/channable/vaultenv/issues/43#issuecomment-521814750

--- static-stack2nix-builder-example/default.nix
+++ static-stack2nix-builder-example/default.nix
@@ -8,11 +8,18 @@ let
   cabalPackageName = "example-project";
   compiler = "ghc864"; # matching stack.yaml
 
-  # Pin nixpkgs version.
-  pkgs = import (fetchTarball https://github.com/nh2/nixpkgs/archive/a2d7e9b875e8ba7fd15b989cf2d80be4e183dc72.tar.gz) {};
-
   # Pin static-haskell-nix version.
-  static-haskell-nix = fetchTarball https://github.com/nh2/static-haskell-nix/archive/1d37d9a83e570eceef9c7dad5c89557f8179a076.tar.gz;
+  static-haskell-nix =
+    if builtins.pathExists ../.in-static-haskell-nix
+      then toString ../. # for the case that we're in static-haskell-nix itself, so that CI always builds the latest version.
+      # Update this hash to use a different `static-haskell-nix` version:
+      else fetchTarball https://github.com/nh2/static-haskell-nix/archive/1d37d9a83e570eceef9c7dad5c89557f8179a076.tar.gz;
+
+  # Pin nixpkgs version
+  # By default to the one `static-haskell-nix` provides, but you may also give
+  # your own as long as it has the necessary patches, using e.g.
+  #     pkgs = import (fetchTarball https://github.com/nh2/nixpkgs/archive/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa123.tar.gz) {};
+  pkgs = import "${static-haskell-nix}/nixpkgs.nix";
 
   stack2nix-script = import "${static-haskell-nix}/static-stack2nix-builder/stack2nix-script.nix" {
     inherit pkgs;
@@ -31,6 +38,7 @@ let
     #!/usr/bin/env bash
     set -eu -o pipefail
     STACK2NIX_OUTPUT_PATH=$(${stack2nix-script})
+    export NIX_PATH=nixpkgs=${pkgs.path}
     ${pkgs.nix}/bin/nix-build --no-link -A static_package --argstr stack2nix-output-path "$STACK2NIX_OUTPUT_PATH" "$@"
   '';
 
