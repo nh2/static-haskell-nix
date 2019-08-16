2f8a75ff8c9c30c27b6d80601922f5757d315800
Niklas Hambüchen <mail@nh2.me>
2019-08-16 03:31:29 +0200

    nixpkgs.nix: Use submodule if present.
    This removes the need to detect HerculesCI specifically,
    because it simply doesn't have the submodule present.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,10 +1,10 @@
-# Detect HerculesCI by checking if NIX_PATH is empty.
-# See https://docs.hercules-ci.com/hercules-ci/getting-started/repository/
-# If it's empty, we give our custom nixpkgs version;
-# otherwise we use what the user has set with NIX_PATH.
-if builtins.getEnv "NIX_PATH" == ""
-  then
-    builtins.trace "NIX_PATH is not set, we're probably in HerculesCI"
-      (import (fetchTarball https://github.com/nh2/nixpkgs/archive/a2d7e9b875e8ba7fd15b989cf2d80be4e183dc72.tar.gz) {})
-  else
-    import <nixpkgs> {}
+# If a `./nixpkgs` submodule exists, use that.
+# Note that this will take precedence over setting NIX_PATH!
+# We prefer this such that `static-stack2nix-builder` and specifically
+# `static-stack2nix-builder-example` can just import `nixpkgs.nix`
+# in CI and when called during development to get the right version of
+# nixpkgs.
+if builtins.pathExists ./nixpkgs/pkgs
+  then import ./nixpkgs {}
+  # Pinned nixpkgs version; should be kept up-to-date with our submodule.
+  else import (fetchTarball https://github.com/nh2/nixpkgs/archive/a2d7e9b875e8ba7fd15b989cf2d80be4e183dc72.tar.gz) {}
