814bf9f9f972c09374fc939847149aa75894af33
Niklas Hambüchen <mail@nh2.me>
2020-02-05 03:10:44 +0100

    Update example commits

--- static-stack2nix-builder-example/default.nix
+++ static-stack2nix-builder-example/default.nix
@@ -13,7 +13,7 @@ let
     if builtins.pathExists ../.in-static-haskell-nix
       then toString ../. # for the case that we're in static-haskell-nix itself, so that CI always builds the latest version.
       # Update this hash to use a different `static-haskell-nix` version:
-      else fetchTarball https://github.com/nh2/static-haskell-nix/archive/81006c805289116811650416a83f5689bed0209b.tar.gz;
+      else fetchTarball https://github.com/nh2/static-haskell-nix/archive/d1b20f35ec7d3761e59bd323bbe0cca23b3dfc82.tar.gz;
 
   # Pin nixpkgs version
   # By default to the one `static-haskell-nix` provides, but you may also give
