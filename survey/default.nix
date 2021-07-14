6e97715e521f4388e53faf3556881e31d9c24bdd
Niklas Hambüchen <mail@nh2.me>
2021-07-14 17:53:33 +0000

    Refactor: Reformat `statify`

--- survey/default.nix
+++ survey/default.nix
@@ -1509,20 +1509,29 @@ let
             # for compiling its `Setup.hs` the Cabal package that comes with GHC
             # (that is in the default GHC package DB) is used instead, which
             # obviously doesn' thave our patches.
-            statify = drv: with final.haskell.lib; final.lib.foldl appendConfigureFlag (disableLibraryProfiling (disableSharedExecutables (useFixedCabal drv))) ([
-              "--enable-executable-static" # requires `useFixedCabal`
-              # `enableShared` seems to be required to avoid `recompile with -fPIC` errors on some packages.
-              "--extra-lib-dirs=${final.ncurses.override { enableStatic = true; enableShared = true; }}/lib"
-            # TODO Figure out why this and the below libffi are necessary.
-            #      `working` and `workingStackageExecutables` don't seem to need that,
-            #      but `static-stack2nix-builder-example` does.
-            ] ++ final.lib.optionals (!integer-simple) [
-              "--extra-lib-dirs=${final.gmp6.override { withStatic = true; }}/lib"
-            ] ++ final.lib.optionals (!integer-simple && approach == "pkgsMusl") [
-              # GHC needs this if it itself wasn't already built against static libffi
-              # (which is the case in `pkgsStatic` only):
-              "--extra-lib-dirs=${final.libffi}/lib"
-            ]);
+            statify = drv:
+              with final.haskell.lib;
+              final.lib.foldl
+                appendConfigureFlag
+                (disableLibraryProfiling (disableSharedExecutables (useFixedCabal drv)))
+                (builtins.concatLists [
+                  [
+                    "--enable-executable-static" # requires `useFixedCabal`
+                    # `enableShared` seems to be required to avoid `recompile with -fPIC` errors on some packages.
+                    "--extra-lib-dirs=${final.ncurses.override { enableStatic = true; enableShared = true; }}/lib"
+                  ]
+                  # TODO Figure out why this and the below libffi are necessary.
+                  #      `working` and `workingStackageExecutables` don't seem to need that,
+                  #      but `static-stack2nix-builder-example` does.
+                  (final.lib.optionals (!integer-simple) [
+                    "--extra-lib-dirs=${final.gmp6.override { withStatic = true; }}/lib"
+                  ])
+                  (final.lib.optionals (!integer-simple && approach == "pkgsMusl") [
+                    # GHC needs this if it itself wasn't already built against static libffi
+                    # (which is the case in `pkgsStatic` only):
+                    "--extra-lib-dirs=${final.libffi}/lib"
+                 ])
+               ]);
         in
           final.lib.mapAttrs
             (name: value:
