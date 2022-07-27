fe82f7fbc0718e7abc4d3bf5eb774c39ecce69a6
Robert Vollmert <rob@vllmrt.net>
2022-07-27 18:52:13 +0200

    bump nixpkgs revision to the latest used in PostgREST

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -25,4 +25,4 @@ if builtins.getEnv "STATIC_HASKELL_NIX_CI_NIXPKGS_UNSTABLE_BUILD" == "1"
     if builtins.pathExists ./nixpkgs/pkgs
       then import ./nixpkgs {}
       # Pinned nixpkgs version; should be kept up-to-date with our submodule.
-      else import (fetchTarball https://github.com/NixOS/nixpkgs/archive/9a5aa75d56ad4163521f1692469e6dc54b90068c.tar.gz) {}
+      else import (fetchTarball https://github.com/NixOS/nixpkgs/archive/8eb14e7e79d6c34a9cce146182bfc8c820d527c2.tar.gz) {}
